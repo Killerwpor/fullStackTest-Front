2002108aaf88d3f58554ab9847c09509b5c82b3b
Johan Suarez <johanc.suarez@hotmail.com>
2021-01-31 19:26:29 -0500

    Feature: base navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+
+
+export class AppComponent  {
   title = 'frontTest';
+  
+
+
+  click(opcion){
+    $(".lineL").hide();
+    $(".lineR").hide();
+    $(".active").removeClass("active");
+
+    switch(opcion){
+      case 1:
+      $("#option1").addClass("active");
+      $(".lineL").show();
+        break;
+      case 2:
+        $("#option2").addClass("active");
+        $(".lineR").show();
+   
+        break;
+    } 
+
+  }
+
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MainComponent } from './main/main.component';
+import { CambioComponent } from './cambio/cambio.component';
+import { ListadoComponent } from './listado/listado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    CambioComponent,
+    ListadoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
